18408c5938cc566dec3cde84af11d3fde51d362f
Northn <erolerol3760@gmail.com>
2021-01-29 11:35:59 +0000

    Settings: Remove build type check

--- src/com/android/settings/system/SystemUpdatePreferenceController.java
+++ src/com/android/settings/system/SystemUpdatePreferenceController.java
@@ -35,7 +35,6 @@ public class SystemUpdatePreferenceController extends BasePreferenceController {
 
     private static final String KEY_SYSTEM_UPDATE_SETTINGS = "system_update_settings";
 
-    private static final String OTA_BUILD_TYPE_PROP = "org.evolution.build_type";
     private static final String OTA_APP_PACKAGE = "org.evolution.ota";
 
     private final UserManager mUm;
@@ -47,8 +46,7 @@ public class SystemUpdatePreferenceController extends BasePreferenceController {
 
     @Override
     public int getAvailabilityStatus() {
-        String buildtype = SystemProperties.get(OTA_BUILD_TYPE_PROP,"unofficial");
-        if (!mUm.isAdminUser() || !buildtype.equalsIgnoreCase("official")){
+        if (!mUm.isAdminUser()) {
             return UNSUPPORTED_ON_DEVICE;
         }
         try {
